21d1be2f04c2611c118b74392974279e6e62c611
rafal-smolaga <rafal.smolaga92@gmail.com>
2026-04-14 19:44:18 +0200

    Add files via upload

---
+++ Skrypty/Remove-RemoteAutoStartRegistry.ps1
@@ -0,0 +1,33 @@
+﻿# Wczytanie listy nazw komputerów z pliku tekstowego
+# Plik powinien zawierać jedną nazwę komputera w każdym wierszu
+$Computers = Get-Content "ścieżka do pliku .txt"
+
+# Rozpoczęcie pętli przechodzącej przez każdy komputer z listy
+ForEach ($Computer in $Computers) {
+    
+    # Wysłanie pojedynczego pinga do komputera (1 pakiet)
+    # -ea silentlycontinue - pomija błędy (np. brak odpowiedzi)
+    $ping = Test-Connection $computer -Count 1 -ea silentlycontinue
+    
+    # Jeśli ping zakończył się sukcesem (komputer odpowiada)
+    if($ping)
+    {
+        # Usunięcie wpisu z rejestru na zdalnym komputerze:
+        # reg delete     - usuwanie wpisu z rejestru
+        # \\$computer\   - ścieżka do zdalnego komputera
+        # HKLM\...\Run\  - klucz autostartu
+        # /v "nazwa"    - nazwa wartości (klucza) do usunięcia
+        # /f             - wymuszenie usunięcia bez potwierdzenia
+        reg delete "\\$computer\HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\" /v "nazwa" /f
+        
+        # Wyświetlenie informacji o powodzeniu na zielonym tle
+        Write-Host "Registry entry removed on $computer" -BackgroundColor "Green"
+    }
+    else
+    {
+        # Wyświetlenie informacji o braku dostępu do komputera na czerwonym tle
+        Write-Host "Computer offline or unreachable: $computer" -BackgroundColor "Red"
+    }
+}
+
+pause
---
+++ Skrypty/Set-RemoteAutoStartRemoteAutoStartRegistry.ps1
@@ -0,0 +1,36 @@
+﻿# Wczytanie listy nazw komputerów z pliku tekstowego
+# Plik powinien zawierać jedną nazwę komputera w każdym wierszu
+$Computers = Get-Content "ścieżka do pliku .txt"
+
+# Rozpoczęcie pętli przechodzącej przez każdy komputer z listy
+ForEach ($Computer in $Computers) {
+    
+    # Wysłanie pojedynczego pinga do komputera (1 pakiet)
+    # -ea silentlycontinue - pomija błędy (np. brak odpowiedzi)
+    # Wynik zapisywany w zmiennej $ping (wartość $true lub $false)
+    $ping = Test-Connection $computer -Count 1 -ea silentlycontinue
+    
+    # Jeśli ping zakończył się sukcesem (komputer odpowiada)
+    if($ping)
+    {
+        # Dodanie wpisu do rejestru na zdalnym komputerze:
+        # reg add        - dodawanie wpisu w rejestrze
+        # \\$computer\   - ścieżka do zdalnego komputera
+        # HKLM\...\Run\  - klucz autostartu (uruchamianie przy starcie systemu)
+        # /f             - wymuszenie nadpisania istniejącego wpisu
+        # /v nazwa       - nazwa wartości (klucza) w rejestrze
+        # /t REG_SZ      - typ wartości (ciąg tekstowy)
+        # /d "ścieżka"   - dane (ścieżka do programu)
+        reg add "\\$computer\HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\" /f /v nazwa /t REG_SZ /d "ścieżka do uruchomienia"
+        
+        # Wyświetlenie informacji o powodzeniu na zielonym tle
+        Write-Host "Registry changed on $computer" -BackgroundColor "Green"
+    }
+    else
+    {
+        # Wyświetlenie informacji o braku dostępu do komputera na czerwonym tle
+        Write-Host "Computer offline or unreachable: $computer" -BackgroundColor "Red"
+    }
+}
+
+pause
